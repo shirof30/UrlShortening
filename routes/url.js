16b8d2863a077b98e69ce287500ed9972f3e37a3
vsistant <vsistant@sfu.ca>
2022-04-14 22:36:17 -0700

    Fixed english

--- routes/url.js
+++ routes/url.js
@@ -42,7 +42,7 @@ router.post('/shorten',async(req, res) => {
 			res.status(500).json('Server error');
 		}
 	} else {
-		res.status(401).json('Invalid input url');
+		res.status(401).json('Invalid URL requested');
 	}
 
 });
