27ced4fd7cedc427b92f502d4ca9a87712e942f5
vsistant <vsistant@sfu.ca>
2022-04-16 01:07:51 -0700

    Fixed code arrangements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import InputField from "./components/InputField"
 const App: React.FC = () => {
-  const handleAdd = (e: React.FormEvent) => {
+  
+  const handleCopy = (e:React.MouseEvent<HTMLElement>) => 
+  {
+    e.preventDefault();
+    navigator.clipboard.writeText(todo); 
+  }
+
+  const [todo, setTodo] = useState<string>("");
+  const handleAdd = (e: React.FormEvent) => 
+  {
     e.preventDefault();
     if(todo) {
-      if (todo.indexOf('https') < 0) {
+      if (todo.indexOf('https') < 0) 
+      {
         //textboxval.value ='TEXT ERROR \nPlease Include the HTTP on the link pasted.\n.... ABORTED ....';
         
         let text1 = "https://";
@@ -24,38 +34,38 @@ const App: React.FC = () => {
           };
 
           //fetch call
-              fetch(url,options).then(response=>response.json())
-          .then(data => {setTodo(data)} );
+          fetch(url,options).then(response=>response.json()).then(data => {setTodo(data)} );
           return;
       } 
-        const url = 'http://localhost:5000/api/url/shorten';
-        const data = JSON.stringify({"longURL": todo});
-        const options=
-            {
-                method: 'POST',
-                headers:{
-                    'Content-Type': 'application/json'
-                },
-                body: data
-            };
 
+      const url = 'http://localhost:5000/api/url/shorten';
+      const data = JSON.stringify({"longURL": todo});
+      const options=
+          {
+              method: 'POST',
+              headers:{
+                  'Content-Type': 'application/json'
+              },
+              body: data
+          };
         //fetch call
         fetch(url,options).then(response=>response.json()).then(data=>{ setTodo(data)});
+        
     }
-  };
-  const handleCopy = (e:React.MouseEvent<HTMLElement>) => 
-  {
-    e.preventDefault();
-    navigator.clipboard.writeText(todo); 
   }
-  const [todo, setTodo] = useState<string>("");
+
+
+
+
   return (
     <div className = "App">
       <span className = "heading"> URL SHORTENING SERVICE </span>
+
       <InputField todo = {todo} 
       setTodo = {setTodo} 
       handleAdd = {handleAdd} 
       handleCopy = {handleCopy}></InputField>
+
     </div>
   );
 }
